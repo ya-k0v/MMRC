bb281829c397c01cb868de547d5d009b4f4edb39
yashka <yashka@users.noreply.github.com>
2026-07-16 14:48:47 +0500

    fix: 10 devices per page

--- public/js/admin.js
+++ public/js/admin.js
@@ -1000,7 +1000,7 @@ function createUsersSection() {
 
     const assigned = new Set(Array.isArray(userDeviceIds) ? userDeviceIds : []);
     let devicePage = 1;
-    const devicePerPage = 20;
+    const devicePerPage = 10;
     let deviceSearch = '';
 
     const renderDeviceList = () => {
